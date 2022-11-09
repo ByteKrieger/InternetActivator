0d5de2e9b852a5c717c66f596f66cee7ab5b8135
ByteKrieger <info@bytekrieger.de>
2022-11-10 00:50:24 +0100

    gui Umgewandelt in tkinter

--- testgui.py
+++ testgui.py
@@ -1,18 +1,58 @@
 #!/usr/bin/python3
-import pathlib
-import pygubu
-PROJECT_PATH = pathlib.Path(__file__).parent
-PROJECT_UI = PROJECT_PATH / "gui.ui"
+import tkinter as tk
+import tkinter.ttk as ttk
 
 
 class GuiApp:
     def __init__(self, master=None):
-        self.builder = builder = pygubu.Builder()
-        builder.add_resource_path(PROJECT_PATH)
-        builder.add_from_file(PROJECT_UI)
+        # build ui
+        toplevel1 = tk.Tk() if master is None else tk.Toplevel(master)
+        toplevel1.configure(background="#95a8ff", height=200, width=200)
+        toplevel1.geometry("300x300")
+        toplevel1.resizable(False, False)
+        label1 = ttk.Label(toplevel1)
+        label1.configure(
+            borderwidth=4,
+            font="{Tondu Beta} 14 {}",
+            text='InternetActivator')
+        label1.place(
+            anchor="nw",
+            relheight=0.10,
+            relwidth=1,
+            relx=0,
+            rely=0,
+            x=0,
+            y=0)
+        self.start = ttk.Button(toplevel1)
+        self.img_icons8spielen100 = tk.PhotoImage(
+            file="start.png")
+        self.start.configure(image=self.img_icons8spielen100, text='button1')
+        self.start.place(
+            anchor="nw",
+            height=155,
+            relheight=0.0,
+            relwidth=0.0,
+            relx=0,
+            rely=0,
+            width=155,
+            x=-5,
+            y=150)
+        self.stop = ttk.Button(toplevel1)
+        self.img_icons8stop100 = tk.PhotoImage(file="stop.png")
+        self.stop.configure(image=self.img_icons8stop100, text='button1')
+        self.stop.place(
+            anchor="nw",
+            height=155,
+            relheight=0.0,
+            relwidth=0.0,
+            relx=0,
+            rely=0,
+            width=155,
+            x=150,
+            y=150)
+
         # Main widget
-        self.mainwindow = builder.get_object("toplevel1", master)
-        builder.connect_callbacks(self)
+        self.mainwindow = toplevel1
 
     def run(self):
         self.mainwindow.mainloop()
