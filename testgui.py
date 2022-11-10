f3ee8fb62a0a8e7bec667f7cd309f66de3f225ae
ByteKrieger <info@bytekrieger.de>
2022-11-10 01:31:24 +0100

    testgui geht nun lässt sich allerdings noch nicht per stop beenden da sich das programm aufhängt

--- testgui.py
+++ testgui.py
@@ -1,11 +1,15 @@
 import tkinter as tk
 import pythonping
+import time
+
 def startping():
-    pythonping.ping('8.8.8.8', verbose=True, count=4 , interval=1)
+    while True:
+        pythonping.ping('8.8.8.8', verbose=True, count=4 , interval=1)
+        time.sleep(5)
 
 root = tk.Tk()
 root.title("Ping")
-root.geometry("300x300")
+root.geometry("66x66")
 root.resizable(False, False)
 
 start = tk.Button(root, text="Start", command=startping)
